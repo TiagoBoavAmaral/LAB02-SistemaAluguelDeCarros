71a5e58da1006347e31ebbc5f1dca6b99ad36cc1
Tiago Boaventura Amaral <tiagoboaventuraamaral@gmail.com>
2025-09-25 16:32:03 -0300

    correcoes

--- rental-system/src/main/resources/data.sql
+++ rental-system/src/main/resources/data.sql
@@ -1,10 +1,10 @@
 -- Senha para todos: 'password' (BCrypt encoded)
 -- Inserir Cliente
-INSERT INTO users (id, email, password, role) VALUES (1, 'cliente@email.com', '$2a$10$3g5A7Oq0.3s1nO5z8uRjR.uA0gU4bZ/x4Z1v3y8E8n.9j2nJ7z5D2', 'ROLE_CLIENTE');
+INSERT INTO users (id, email, password, role) VALUES (1, 'cliente@email.com', '$2a$10$I4lD2pfnimOEsoOdI.xJxeizuBvRi1dtTkDu5buJWZxXCWyds8Dp.', 'ROLE_CLIENTE');
 INSERT INTO cliente (id, nome, rg, cpf, endereco, profissao) VALUES (1, 'Joao Cliente', 'MG-123456', '123.456.789-00', 'Rua A, 123', 'Engenheiro');
 
 -- Inserir Agente
-INSERT INTO users (id, email, password, role) VALUES (2, 'agente@email.com', '$2a$10$3g5A7Oq0.3s1nO5z8uRjR.uA0gU4bZ/x4Z1v3y8E8n.9j2nJ7z5D2', 'ROLE_AGENTE');
+INSERT INTO users (id, email, password, role) VALUES (2, 'agente@email.com', '$2a$10$I4lD2pfnimOEsoOdI.xJxeizuBvRi1dtTkDu5buJWZxXCWyds8Dp.', 'ROLE_AGENTE');
 INSERT INTO agente (id, nome_empresa) VALUES (2, 'Banco Fictício S/A');
 
 -- Inserir Automóveis
